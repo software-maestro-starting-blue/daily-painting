cbf72d7a4b19448ca2272e30e41ea58f4ae870f8
SproutMj <mjk54565@gmail.com>
2024-06-18 20:41:17 +0900

    init: 스프링 프로젝트 초기화

---
+++ backend/src/main/java/com/startingblue/dailypainting/DailypaintingApplication.java
@@ -0,0 +1,13 @@
+package com.startingblue.dailypainting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailypaintingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailypaintingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/startingblue/dailypainting/DailypaintingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startingblue.dailypainting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailypaintingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
