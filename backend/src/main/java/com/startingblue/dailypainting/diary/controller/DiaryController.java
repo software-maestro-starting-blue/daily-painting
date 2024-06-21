902a73d17852ebd58e02d5aec6f6633054e1eaae
minseo Jo <liging12@naver.com>
2024-06-21 18:06:28 +0900

    feat: 일기 작성 API 개발 (#6)
    * feat: 일기 작성 API 개발
    
    Diary, ChatGPT (DALLE 3)
    
    * fix: 일기 작성 API 개발
    
    - @GetMapping("/api/diaries")
    이전: 각 날씨, 감정, 만난 사람의 타입을 한 번에 Arrays.toStirng()으로 묶어서 보내줌
    
    변경: 한 번에 묶어서 보내주는 것이 아닌 각 타입별로 따로 보내줌
    
    * refactor: 일기 작성 API 개발
    
    - 상수화
    - 주석 제거
    - 메서드 분리
    
    조금 더 수정 해야함

--- backend/src/main/java/com/startingblue/dailypainting/diary/controller/DiaryController.java
+++ backend/src/main/java/com/startingblue/dailypainting/diary/controller/DiaryController.java
@@ -1,7 +1,53 @@
 package com.startingblue.dailypainting.diary.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.startingblue.dailypainting.openai.service.OpenAIService;
+import com.startingblue.dailypainting.diary.domain.Emotion;
+import com.startingblue.dailypainting.diary.domain.MetPerson;
+import com.startingblue.dailypainting.diary.domain.Weather;
+import com.startingblue.dailypainting.diary.dto.DiarySaveRequest;
+import com.startingblue.dailypainting.diary.dto.DiaryFormResponse;
+import com.startingblue.dailypainting.diary.dto.DiarySavedResponse;
+import com.startingblue.dailypainting.diary.service.DiaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
+@RequiredArgsConstructor
 public final class DiaryController {
+
+    private final DiaryService diaryService;
+    private final OpenAIService openAIService;
+
+    @GetMapping("/api/diaries")
+    public ResponseEntity<DiaryFormResponse> diaryHome() {
+        DiaryFormResponse diaryFormResponse = new DiaryFormResponse(
+                Weather.findAllNames(),
+                Emotion.findAllNames(),
+                MetPerson.findAllNames());
+
+        return ResponseEntity.ok(diaryFormResponse);
+    }
+
+    @PostMapping("/api/diaries")
+    public ResponseEntity<DiarySavedResponse> createDiary(@RequestBody DiarySaveRequest diarySaveRequest,
+                                                          UriComponentsBuilder uriBuilder) {
+        Long savedDiaryId = diaryService.save(diarySaveRequest);
+
+        CompletableFuture<String> future = openAIService.sendDiaryToChatGpt(diarySaveRequest.getContent());
+        String imageUrl = future.join();
+
+        diaryService.updateDiaryImagePath(savedDiaryId, imageUrl);
+
+        DiarySavedResponse diarySavedResponse = new DiarySavedResponse(savedDiaryId, imageUrl);
+        URI location = uriBuilder.path("/diaries/show")
+                .build()
+                .toUri();
+
+        return ResponseEntity.created(location).body(diarySavedResponse);
+    }
 }
--- backend/src/main/java/com/startingblue/dailypainting/diary/domain/Diary.java
+++ backend/src/main/java/com/startingblue/dailypainting/diary/domain/Diary.java
@@ -1,4 +1,62 @@
 package com.startingblue.dailypainting.diary.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Getter
 public final class Diary {
+
+    @Id @GeneratedValue
+    @Column(name = "diary_id")
+    private Long id;
+
+    // TODO: VO로 수정 가능
+    private int birthYear;
+
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private Weather weather;
+
+    @ElementCollection(targetClass = Emotion.class)
+    @Enumerated(EnumType.STRING)
+    private List<Emotion> emotions;
+
+    @ElementCollection(targetClass = MetPerson.class)
+    @Enumerated(EnumType.STRING)
+    @Column(name = "met_people")
+    private List<MetPerson> metPeople;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(columnDefinition = "TEXT")
+    private String imageUrl;
+
+    protected Diary() {
+    }
+
+    private Diary(final int birthYear, final Gender gender,
+                  final Weather weather, final List<Emotion> emotions, final List<MetPerson> metPeople,
+                  final String content) {
+        this.birthYear = birthYear;
+        this.gender = gender;
+        this.weather = weather;
+        this.emotions = emotions;
+        this.metPeople = metPeople;
+        this.content = content;
+    }
+
+    public static Diary createDiary(final int birthYear, final Gender gender,
+                                    final Weather weather, final List<Emotion> emotions, final List<MetPerson> metPeople,
+                                    final String content) {
+        return new Diary(birthYear, gender, weather, emotions, metPeople, content);
+    }
+
+    public void updateImageUrl(final String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
