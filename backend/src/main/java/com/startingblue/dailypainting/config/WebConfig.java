9cfeee8cd98e13ec17270dfd1558b780aa93d487
JuneBridge <junebridge03@g.skku.edu>
2024-07-04 12:41:40 +0900

    fix: Update WebConfig for CORS problem

--- backend/src/main/java/com/startingblue/dailypainting/config/WebConfig.java
+++ backend/src/main/java/com/startingblue/dailypainting/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.startingblue.dailypainting.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Value("${cors.allowed.origin}")
+    private String allowedOrigin;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(allowedOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Location")
