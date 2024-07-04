7b47640999a679efacbb8c9537bfe698416e2ce8
JuneBridge <junebridge03@g.skku.edu>
2024-07-04 17:03:01 +0900

    feat: S3 저장 기능 추가

--- backend/src/main/java/com/startingblue/dailypainting/diary/service/DiaryService.java
+++ backend/src/main/java/com/startingblue/dailypainting/diary/service/DiaryService.java
@@ -5,9 +5,17 @@ import com.startingblue.dailypainting.diary.dto.DiarySaveRequest;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StreamUtils;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 @Service
@@ -16,7 +24,15 @@ import java.util.List;
 @Transactional
 public class DiaryService {
 
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    @Value("${aws.s3.diary-image-folder}")
+    private String diaryImageFolder;
+
     private final DiaryRepository diaryRepository;
+
+    private final S3Client s3Client;
 
     public Long save(final DiarySaveRequest diarySaveRequest) {
         int birthYear = diarySaveRequest.getBirthYear();
@@ -40,11 +56,43 @@ public class DiaryService {
         return savedDiary.getId();
     }
 
-    public void updateDiaryImagePath(final Long diaryId, final String imageUrl) {
+    public void updateDiaryImage(final Long diaryId, final String imageUrl) {
+        // 이미지 업로드
+        updateDiaryImagePath(diaryId, imageUrl);
+        uploadDiaryImageS3(diaryId, imageUrl);
+    }
+
+    private void updateDiaryImagePath(final Long diaryId, final String imageUrl) {
             Diary diary = diaryRepository.findById(diaryId)
                     .orElseThrow(() -> new EntityNotFoundException("Diary with ID " + diaryId + " not found"));
 
             diary.updateImageUrl(imageUrl);
             diaryRepository.save(diary);
+    }
+
+    private void uploadDiaryImageS3(final Long diaryId, final String imageUrl) {
+        byte[] imageBytes = downloadDiaryImageFromUrl(imageUrl);
+        final String uploadPath = diaryImageFolder + diaryId.toString() + ".jpg";
+        final PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .key(uploadPath)
+                .contentType("image/*")
+                .bucket(bucketName)
+                .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromBytes(imageBytes));
+    }
+
+    private byte[] downloadDiaryImageFromUrl(final String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            InputStream inputStream = connection.getInputStream();
+            return StreamUtils.copyToByteArray(inputStream);
+        } catch (Exception e) {
+            log.error("Failed to download image from URL: {}", imageUrl, e);
+        }
+        return new byte[0];
     }
 }
